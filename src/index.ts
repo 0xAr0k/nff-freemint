3a41fa502073248a19a02bbb7e3b4f98825e3c61
0xar0k <arok.eth@proton.me>
2025-11-28 00:04:56 +0700

    log errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import app from "./app";
 import { env } from "./env";
+import { logger } from "./logger";
+
+logger.info("Starting server...");
+logger.info(`PORT: ${env.PORT}`);
+logger.info(`REDIS_REST_URL: ${env.UPSTASH_REDIS_REST_URL}`);
 
 const port = env.PORT;
 
