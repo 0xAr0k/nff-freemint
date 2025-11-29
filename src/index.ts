782a6e3f0987f599b34c25a8ab4d89adfcf01473
0xar0k <arok.eth@proton.me>
2025-11-30 05:58:43 +0700

    remove logs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import app from "./app";
 import { env } from "./env";
-import { logger } from "./logger";
-
-logger.info("Starting server...");
-logger.info(`PORT: ${env.PORT}`);
-logger.info(`REDIS_REST_URL: ${env.UPSTASH_REDIS_REST_URL}`);
 
 const port = env.PORT;
 
